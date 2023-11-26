603c58fac864a22314c4ac9d6fe5d14baee4d1d0
pasindu uduwela <puduwela@icptechno.com>
2023-11-26 22:52:39 +0530

    works committed

---
+++ src/main/java/com/example/SpringSecurity/JWT/config/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package com.example.SpringSecurity.JWT.config;
+
+import com.example.SpringSecurity.JWT.entities.Role;
+import com.example.SpringSecurity.JWT.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    private final UserService userService;
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws  Exception{
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request.requestMatchers("/api/v1/auth/**")
+                        .permitAll()
+                        .requestMatchers("/api/v1/admin").hasAnyAuthority(Role.ADMIN.name())
+                        .requestMatchers("/api/v1/user").hasAnyAuthority(Role.USER.name())
+                        .anyRequest().authenticated()
+
+
+                );
+        return http.build();
+    }
+}
