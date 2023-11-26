f27ce92b1eb87ebe24759146e6102d3711a7077b
pasindu uduwela <puduwela@icptechno.com>
2023-11-26 22:43:09 +0530

    works committed

---
+++ src/main/java/com/example/SpringSecurity/JWT/services/impl/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.SpringSecurity.JWT.services.impl;
+
+import com.example.SpringSecurity.JWT.repository.UserRepository;
+import com.example.SpringSecurity.JWT.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+
+    @Override
+    public UserDetailsService userDetailsService (){
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return userRepository.findByEmail(username).orElseThrow(()-> new UsernameNotFoundException("User not found"));
+            }
+        };
+    }
+}
